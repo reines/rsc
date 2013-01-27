30b05f2fa9154b3b6a7eb2c554788569d5931928
Jamie Furness <jfurness@yammer-inc.com>
2013-01-27 22:46:25 +0000

    Adding a toString on session requests

--- server/src/main/java/com/jamierf/rsc/server/net/packet/SessionRequestPacket.java
+++ server/src/main/java/com/jamierf/rsc/server/net/packet/SessionRequestPacket.java
@@ -1,9 +1,16 @@
 package com.jamierf.rsc.server.net.packet;
 
+import com.google.common.base.Objects;
 import com.jamierf.rsc.server.net.codec.packet.Packet;
 
 public class SessionRequestPacket extends Packet {
 
     private byte sessionId;
 
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                .add("sessionId", sessionId)
+                .toString();
+    }
 }
